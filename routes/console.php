359789716539b0d8d1a9d15595b04e9b78ec2450
vikash_diwakar <vikashdiwakar777@gmail.com>
2026-09-06 00:40:13 +0530

    Schedule queue worker in routes/console.php

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Process background queue jobs automatically every minute
+Schedule::command('queue:work --stop-when-empty --tries=3')
+    ->everyMinute()
+    ->withoutOverlapping();
+
